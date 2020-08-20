9c0ea82ba0b4b2948b2cf3ee4989207efc19d01b
lapolakalu <titteringtonpaola@gmail.com>
2020-08-20 10:44:38 +0200

    csv interne

--- config.js
+++ config.js
@@ -2,18 +2,18 @@
 const config = {
     style: "mapbox://styles/paolati/ckcdrbphi08jd1jo47fh3b6n9",
     accessToken: "pk.eyJ1IjoicGFvbGF0aSIsImEiOiJja2QxZHc1YXkwYmVxMnpud2JhbThoYmVhIn0.rTRvxjSc-D398zOrFRYILw",
-    CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRBqho-7XLVIzxWoQTdgCqxzT4cUiDwYE9YiH9WaTkY4Oozc0UsJaGBxx6doZNHguQbPgmOhU0Dgmft/pub?gid=485374184&single=true&output=csv",
+    CSV: "./donnes.csv",
     center: [4.8406743, 45.7531474], //Lng, Lat
     zoom: 6, //Default zoom
     title: "Biennale Traces",
     description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
-    sideBarInfo: ["Nom","Description","Date","Contact","Site Internet"],
-    popupInfo: ["Nom"],
+    sideBarInfo: ["Soiree","Thematique","Description","Forme","Date","Horaire"],
+    popupInfo: ["Soiree"],
     filters: [
         {
             type: "checkbox",
-            title: "Thématique : ",
-            columnHeader: "Thématique",
+            title: "Thematique : ",
+            columnHeader: "Thematique",
             listItems: ["Travail","Habitat", "Expression artistique des personnes migrantes","Autre"]
         },
         {
