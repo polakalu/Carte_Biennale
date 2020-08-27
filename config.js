3ccb1b807dc273dcbc0c2af00a01be36bc2d4953
lapolakalu <titteringtonpaola@gmail.com>
2020-08-27 10:34:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 const config = {
-    style: "mapbox://styles/paolati/cke8bqy1448cg19ljsleo5877",
-    accessToken: "mapbox://styles/paolati/ckecjrk222ai61amvhjgt21va",
+    style: "mapbox://styles/paolati/ckecjrk222ai61amvhjgt21va",
+    accessToken: "pk.eyJ1IjoicGFvbGF0aSIsImEiOiJja2QxZHc1YXkwYmVxMnpud2JhbThoYmVhIn0.rTRvxjSc-D398zOrFRYILw",
     CSV: "./donnees.csv",
     center: [4.8406743, 45.7531474], //Lng, Lat
     zoom: 6, //Default zoom
