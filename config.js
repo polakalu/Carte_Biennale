a50ea6299c09075d27438a58cf5e1f562b22331e
lapolakalu <titteringtonpaola@gmail.com>
2020-09-08 17:06:40 +0200

    test

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const config = {
     style: "mapbox://styles/paolati/ckecjrk222ai61amvhjgt21va",
     accessToken: "pk.eyJ1IjoicGFvbGF0aSIsImEiOiJja2QxZHc1YXkwYmVxMnpud2JhbThoYmVhIn0.rTRvxjSc-D398zOrFRYILw",
-    CSV: "./PROGRAMME.csv",
+    CSV: "./PROGRAMME3.csv",
     center: [4.8406743, 45.7531474], //Lng, Lat
     zoom: 6, //Default zoom
     title: "Biennale Traces",
