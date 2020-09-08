6b71f2457399814b24856320549bec36c3b8e2f1
lapolakalu <titteringtonpaola@gmail.com>
2020-09-08 11:38:56 +0200

    programme update code

--- config.js
+++ config.js
@@ -2,19 +2,19 @@
 const config = {
     style: "mapbox://styles/paolati/ckecjrk222ai61amvhjgt21va",
     accessToken: "pk.eyJ1IjoicGFvbGF0aSIsImEiOiJja2QxZHc1YXkwYmVxMnpud2JhbThoYmVhIn0.rTRvxjSc-D398zOrFRYILw",
-    CSV: "./donnees.csv",
+    CSV: "./PROGRAMME.csv",
     center: [4.8406743, 45.7531474], //Lng, Lat
     zoom: 6, //Default zoom
     title: "Biennale Traces",
     description: "La Biennale Traces qui aura lieu du 7 octobre au 7 décembre 2020, regroupe une centaine de propositions (conférences, tables rondes, expositions, journées d'études, projections de films, concerts, spectacles). Ces temps, sont portées par une grande diversité d’acteurs, qui tous ont en commun de travailler les questions qui entourent les migrations d’hier et d’aujourd’hui en région Auvergne-Rhône-Alpes : chercheur.euses, artistes, médias, collectifs et associations, lieux et institutions, collectivités territoriales, etc.. ",
-    sideBarInfo: ["Soiree","Forme","Date","Horaire","Thematique","Description"],
-    popupInfo: ["Soiree"],
+    sideBarInfo: ["Titre","Forme","Date","Horaire","Thematique","Description"],
+    popupInfo: ["Titre"],
     filters: [
         {
             type: "checkbox",
             title: "Thematique : ",
             columnHeader: "Thematique",
-            listItems: ["Travail","Habitat", "Expression artistique des personnes migrantes","Autre"]
+            listItems: ["Travail","Habitat", "Expression artistique des personnes migrantes",]
         },
         {
             type: "dropdown",
@@ -36,7 +36,7 @@ const config = {
             type: "checkbox",
             title: "Type de soirée: ",
             columnHeader: "Forme",
-            listItems: ["Film", "Conférence", "Théâtre","Table ronde","Concert","Exposition"]
+            listItems: ["Film", "Conférence", "Théâtre","table ronde","Concert","Exposition","débat"]
         }
     ]
 
