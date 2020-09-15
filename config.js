fd6f3e73e4e7ef0abd37624fab5051dc562d16d9
lapolakalu <titteringtonpaola@gmail.com>
2020-09-15 17:17:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
     accessToken: "pk.eyJ1IjoicGFvbGF0aSIsImEiOiJja2QxZHc1YXkwYmVxMnpud2JhbThoYmVhIn0.rTRvxjSc-D398zOrFRYILw",
     CSV: "./PROGRAMME3.csv",
     center: [4.8406743, 45.7531474], //Lng, Lat
-    zoom: 7, //Default zoom
+    zoom: 8, //Default zoom
     title: "Biennale Traces",
     description: "La Biennale Traces qui aura lieu du 7 octobre au 7 décembre 2020, regroupe une centaine de propositions (conférences, tables rondes, expositions, journées d'études, projections de films, concerts, spectacles). Ces temps, sont portées par une grande diversité d’acteurs, qui tous ont en commun de travailler les questions qui entourent les migrations d’hier et d’aujourd’hui en région Auvergne-Rhône-Alpes : chercheur.euses, artistes, médias, collectifs et associations, lieux et institutions, collectivités territoriales, etc.. ",
     sideBarInfo: ["Titre","Thematique","Forme","Date","Horaire","Description"],
