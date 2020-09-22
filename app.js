eb2498d7a83ccb5fc3b0c02c9c6e3a20442bed45
martees <60579713+martees@users.noreply.github.com>
2020-09-22 14:24:59 +0200

    Update app.js
    début image + bouton

--- app.js
+++ app.js
@@ -21,15 +21,28 @@ function flyToLocation(currentFeature, zoom) {
     });
 }
 
+//POPUP
 function createPopup(currentFeature) {
-    const popups = document.getElementsByClassName("mapboxgl-popup");
-    /** Check if there is already a popup on the map and if so, remove it **/
-    if (popups[0]) popups[0].remove();
-    const popup = new mapboxgl.Popup({ closeOnClick: true })
-        .setLngLat(currentFeature.geometry.coordinates)
-        .setHTML("<h3>" + currentFeature.properties[config.popupInfo] + "</h3>")
-        .addTo(map);
+const popups = document.getElementsByClassName("mapboxgl-popup");
+/** site web **/
+let empt = ""
+if (currentFeature.properties[config.popupWeb]){
+  empt = '<div class="flex-parent flex-parent--center-main"><button class="btn btn--#dd520"><a href="'+ currentFeature.properties[config.popupWeb]
+  +'" target="_blank" rel="noopener noreferrer" > En savoir + </a></button></div><br>';}
+const site = empt;
+/** Text in the popup */
+const informations =`<h3>` + currentFeature.properties[config.popupNom] + `</h3>`
+                    + `<h4>`+ currentFeature.properties[config.popupDescrip] + `</h4>`
+                    + site;
+/** Check if there is already a popup on the map and if so, remove it */
+if (popups[0]) popups[0].remove();
+const popup = new mapboxgl.Popup({ closeOnClick: true })
+    .setLngLat(currentFeature.geometry.coordinates)
+    //.setHTML(' <img src=/images/" ' + '' + ' "> ' + "<h3>" + currentFeature.properties[config.popupInfo] + "</h3>")
+    .setHTML(informations)
+    .addTo(map);
 }
+
 
 function buildLocationList(locationData) {
     /* Add a new listing section to the sidebar. */
