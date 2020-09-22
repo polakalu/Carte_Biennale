47904c37fde421d04d6a57ada53812765762f657
martees <60579713+martees@users.noreply.github.com>
2020-09-22 18:06:56 +0200

    import liens
    import liens

--- config.js
+++ config.js
@@ -2,12 +2,13 @@
 const config = {
     style: "mapbox://styles/paolati/cke8bqy1448cg19ljsleo5877",
     accessToken: "pk.eyJ1IjoicGFvbGF0aSIsImEiOiJja2QxZHc1YXkwYmVxMnpud2JhbThoYmVhIn0.rTRvxjSc-D398zOrFRYILw",
-    CSV: "./PROGRAMME3.csv",
+    CSV: "./PROGRAMME.csv",
     center: [4.825780, 45.477416], //Lng, Lat
     zoom: 7, //Default zoom
     title: "Biennale Traces",
     description: "La Biennale Traces qui aura lieu du 7 octobre au 7 décembre 2020, regroupe une centaine de propositions (conférences, tables rondes, expositions, journées d'études, projections de films, concerts, spectacles). Ces temps, sont portées par une grande diversité d’acteurs, qui tous ont en commun de travailler les questions qui entourent les migrations d’hier et d’aujourd’hui en région Auvergne-Rhône-Alpes : chercheur.euses, artistes, médias, collectifs et associations, lieux et institutions, collectivités territoriales, etc.. ",
     sideBarInfo: ["Titre","Dates","Thematique","Forme","Horaire","Description"],
+    popupWeb: ["Lien"]
     filters: [
         {
             type: "checkbox",
